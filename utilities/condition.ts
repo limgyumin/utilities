6d9d1c2d418798c0547e2a8c8975c414d5811016
limgyumin <dev.limgyumin@gmail.com>
2023-08-27 05:03:55 +0900

    refactor: condition 함수 시그니처의 단항 함수를 Func -> Unary로 타이핑

--- utilities/condition.ts
+++ utilities/condition.ts
@@ -7,8 +7,8 @@ import type { Guard } from "~types/Guard";
 import { isFunction } from "./isFunction";
 
 type Condition = {
-  <P, S extends P, R1>(condition: Guard<P, S>, truthy: Func<[S], R1> | R1): Unary<P, R1>;
-  <P, S extends P, R1, R2>(condition: Guard<P, S>, truthy: Func<[S], R1> | R1, falsy: Func<[P], R2>): Unary<P, R1 | R2>;
+  <P, S extends P, R1>(condition: Guard<P, S>, truthy: Unary<S, R1> | R1): Unary<P, R1>;
+  <P, S extends P, R1, R2>(condition: Guard<P, S>, truthy: Unary<S, R1> | R1, falsy: Unary<P, R2>): Unary<P, R1 | R2>;
 
   <P extends any[], R1>(condition: Func<P, boolean> | boolean, truthy: Func<P, R1> | R1): Func<P, R1>;
   <P extends any[], R1, R2>(condition: Func<P, boolean> | boolean, truthy: Func<P, R1> | R1, falsy: Func<P, R2> | R2): Func<P, R1 | R2>;
